369a9081eda371edfd457fec0a545009dd5dfdab
J. Correia <joanaqcorreia@gmail.com>
2023-04-17 23:45:19 +0100

    Populated meal arrays

--- Random message.js
+++ Random message.js
@@ -1,6 +1,6 @@
 // the parts of the meal to be generated
-const appt = [];
-const main = [];
-const side = [];
-const dessrt = [];
+const appt = ["charcuterie Board", "guacamole and chips", "pork sliders", "cheese biscuits", "scones", "pretzel bites", "rissois", "fried shrimp", "chilli fries"];
+const main = ["lamb shanks", "lasagna", "ham pizza", "tuna casserole", "deep fried steak", "swedish meatballs", "sauteed salmon", "fried fish", "grilled sardines", "cheeseburger", "baked tilapia"];
+const side = ["fried rice", "spaghetti", "white rice", "boiled potatoes", "coleslaw", "country fries", "grilled green beans", "onion rings", "baked sweet potato", "roasted jalapeño peppers", "risotto"];
+const dessrt = ["cheesecake","chocolate bundt cake", "crepes", "churro bites", "frosted cupcakes", "brownies", "chunky chocolate chip cookies", "oreo mousse", "pancakes", "pumpkin pie"];
 
