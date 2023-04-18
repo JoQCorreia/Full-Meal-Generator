7220f40082d7006968a5d06f9fad9cafb90ed87d
J. Correia <joanaqcorreia@gmail.com>
2023-04-18 01:04:33 +0100

    Added alternate meal generator

--- Random message.js
+++ Random message.js
@@ -1,4 +1,4 @@
-// the parts of the meal to be generated
+// arrays with the parts of the meal to be generated
 const appt = ["charcuterie board", "guacamole and chips", "pork sliders", "cheese biscuits", "scones", "pretzel bites", "rissois", "fried shrimp", "chilli fries"];
 const main = ["lamb shanks", "lasagna", "ham pizza", "tuna casserole", "deep fried steak", "swedish meatballs", "sauteed salmon", "fried fish", "grilled sardines", "cheeseburger", "baked tilapia"];
 const side = ["fried rice", "spaghetti", "white rice", "boiled potatoes", "coleslaw", "country fries", "grilled green beans", "onion rings", "baked sweet potato", "roasted jalapeño peppers", "risotto"];
@@ -14,12 +14,50 @@ const yourMeal = () => {
     const randomSide = Math.floor(Math.random() * (side.length));
     const randomDessert = Math.floor(Math.random() * (dessrt.length));
 
+    // generates an alternate meal suggestion without any repeats
+
+    const altAppt = () =>{
+        let arr = Math.floor(Math.random() * (appt.length));
+        while (arr == randomAppt) {
+            arr = Math.floor(Math.random() * (appt.length));
+        }
+        let newAppt = appt[arr];
+        return newAppt;
+    };
+    const altMain = () => {
+        let arr = Math.floor(Math.random() * (main.length));
+        while (arr == randomMain) {
+            arr = Math.floor(Math.random() * (main.length));
+        };
+        let newMain = main[arr];
+        return newMain;
+    };
+
+    const altSide = () => {
+        
+        let arr = Math.floor(Math.random() * (side.length));
+        while (arr == randomSide) {
+            arr = Math.floor(Math.random() * (side.length));
+        }
+        let newSide = side[arr];
+        return newSide;
+    };
+    
+    const altDessert = () => {
+        let arr = Math.floor(Math.random() * (dessrt.length));
+        while (arr == randomDessert) {
+            arr = Math.floor(Math.random() * (dessrt.length));
+        }
+        let newDessr = dessrt[arr];
+        return newDessr;
+    }
+
 
     // composes final meal suggestions
 
     message = `Hungry but fresh out of ideas? How about... Start strong with ${appt[randomAppt]} then ${main[randomMain]} with ${side[randomSide]} and end it with ${dessrt[randomDessert]}!`;
 
-    console.log(message);
+
 
 };
 
